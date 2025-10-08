92096b273a9ef9d5be87eb2d898cbe0a55cbddfa
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-08 23:31:56 +0200

    añade Ej0198 tema05

--- programas/Ejercicios/tema05/src/Ej019.java
+++ programas/Ejercicios/tema05/src/Ej019.java
@@ -5,5 +5,34 @@ pedir por teclado. El carácter con el que se pinta la pirámide también se deb
 pedir por teclado.
  */
 
+import java.util.Scanner;
+
 public class Ej019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce la altura de la pirámide: ");
+        int altura = sc.nextInt();
+
+        System.out.print("Introduce el caracter de la pirámide: ");
+        String simbolo = sc.next();
+
+        for (int i = 1; i <= altura ; i++) {
+            for (int j = 1; j <= altura - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Imprimir caracteres (crecen)
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print(simbolo);
+            }
+
+            // 3Salto de línea
+            System.out.println();
+        }
+
+        sc.close();
+
+    }
+
 }

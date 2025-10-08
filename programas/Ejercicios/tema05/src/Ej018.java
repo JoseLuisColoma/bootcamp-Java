8692f1a5955b28d49f4f4e7d667d0615545de87b
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-08 23:17:33 +0200

    añade Ej018 tema05

--- programas/Ejercicios/tema05/src/Ej018.java
+++ programas/Ejercicios/tema05/src/Ej018.java
@@ -5,5 +5,47 @@ números introducidos por teclado y validados como distintos, el programa debe
 empezar por el menor de los enteros introducidos e ir incrementando de 7 en 7.
  */
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Ej018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        int numero1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int numero2 = sc.nextInt();
+
+        if (numero1 == numero2) {
+            System.out.println("Los números son iguales. Prueba de nuevo.");
+            sc.close();
+            return;
+        }
+
+        // Intercambiar si el primero es mayor
+        if (numero1 > numero2) {
+            int temp = numero1;
+            numero1 = numero2;
+            numero2 = temp;
+        }
+
+        System.out.println("\nNúmeros comprendidos entre " + numero1 + " y " + numero2 + " (incrementando de 7 en 7):");
+
+        boolean primero = true; // para controlar la primera coma
+
+        for (int i = numero1 + 7; i < numero2; i += 7) {
+            if (!primero) {
+                System.out.print(", ");
+            }
+            System.out.print(i);
+            primero = false;
+        }
+
+        System.out.println(); // salto de línea final
+        sc.close();
+    }
 }
+

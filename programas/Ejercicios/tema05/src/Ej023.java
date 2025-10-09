ba1b891a92764a9afcca6f0f6b1456f7d3e6f30c
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-09 23:57:28 +0200

    añade Ej023 tema05

--- programas/Ejercicios/tema05/src/Ej023.java
+++ programas/Ejercicios/tema05/src/Ej023.java
@@ -6,5 +6,35 @@ ocurra, se debe mostrar el total acumulado, el contador de los números
 introducidos y la media.
  */
 
+import java.util.Scanner;
+
 public class Ej023 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int LIMITE = 10000;
+        int suma = 0;
+        int contador = 0;
+
+        System.out.println("Introduce números enteros (el programa se detendrá al superar 10.000):");
+
+        while (suma < LIMITE) {
+            System.out.print("Número: ");
+            int numero = sc.nextInt();
+
+            suma += numero;
+            contador++;
+        }
+
+        double media = (double) suma / contador;
+
+        System.out.println("\n==============================");
+        System.out.println("Total acumulado: " + suma);
+        System.out.println("Cantidad de números introducidos: " + contador);
+        System.out.printf("Media: %.2f%n", media);
+        System.out.println("==============================");
+
+        sc.close();
+    }
 }

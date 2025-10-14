332fba45d25cf18744cfd63a5f8e555a89759404
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-14 22:43:35 +0200

    añade Ej006 tema07

--- programas/Ejercicios/tema07/src/Ej006.java
+++ programas/Ejercicios/tema07/src/Ej006.java
@@ -7,5 +7,32 @@ la última posición debe pasar a la posición 0. Finalmente, muestra el conteni
 del array.
  */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ej006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int CANTIDAD_NUMEROS = 15; // según el enunciado
+        int[] numeros = new int[CANTIDAD_NUMEROS];
+        int[] numerosRotados = new int[CANTIDAD_NUMEROS];
+
+        // Entrada de datos
+        for (int i = 0; i < CANTIDAD_NUMEROS; i++) {
+            System.out.print("Introduce un número (" + (i + 1) + "/" + CANTIDAD_NUMEROS + "): ");
+            numeros[i] = sc.nextInt();
+        }
+
+        // Rotar el array una posición a la derecha
+        numerosRotados[0] = numeros[CANTIDAD_NUMEROS - 1];
+        for (int i = 0; i < CANTIDAD_NUMEROS - 1; i++) {
+            numerosRotados[i + 1] = numeros[i];
+        }
+
+        // Mostrar resultados
+        System.out.println("\nArray original: " + Arrays.toString(numeros));
+        System.out.println("Array rotado:   " + Arrays.toString(numerosRotados));
+
+        sc.close();
+    }
 }

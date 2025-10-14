81372336c4a788358be6c743beeb096e2d188491
Jose Luis Coloma <colomatormojl@gmail.com>
2025-10-14 22:14:24 +0200

    añade Ej005 tema07

--- programas/Ejercicios/tema07/src/Ej005.java
+++ programas/Ejercicios/tema07/src/Ej005.java
@@ -5,5 +5,40 @@ números introducidos junto con las palabras “máximo” y “mínimo” al la
 máximo y del mínimo respectivamente.
  */
 
+import java.util.Scanner;
+
 public class Ej005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int CANTIDAD_NUMEROS = 10;
+        int[] numeros = new int[CANTIDAD_NUMEROS];
+
+        for (int i = 0; i < CANTIDAD_NUMEROS; i++) {
+            System.out.print("Introduce un número (" + (i + 1) + "/" + CANTIDAD_NUMEROS + "): ");
+            numeros[i] = sc.nextInt();
+        }
+            int maximo = numeros[0];
+            int minimo = numeros[0];
+
+        for (int i = 0; i < CANTIDAD_NUMEROS; i++) {
+            if (numeros[i] > maximo) {
+                maximo = numeros[i];
+            }
+            if (numeros[i] < minimo) {
+                minimo = numeros[i];
+            }
+        }
+
+        System.out.println("\n--- RESULTADOS ---");
+        for (int i = 0; i < CANTIDAD_NUMEROS; i++) {
+            if (numeros[i] == maximo) {
+                System.out.println(numeros[i] + "  (máximo)");
+            } else if (numeros[i] == minimo) {
+                System.out.println(numeros[i] + "  (mínimo)");
+            } else {
+                System.out.println(numeros[i]);
+            }
+        }
+        sc.close();
+    }
 }
